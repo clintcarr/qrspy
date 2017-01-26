d34eb85f1f41fa395f500ba1cd48222fc8e67203
Clint Carr <clint.carr@outlook.com>
2017-01-26 13:37:33 +1100

    Fixed bug in export_app

--- qrspy.py
+++ qrspy.py
@@ -98,24 +98,14 @@ class ConnectQlik:
             headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
         if filterparam is None:
             if '?' in endpoint:
-                initialconnection = session.get('https://{0}/{1}&xrfkey={2}'.format (self.server, endpoint, xrf),
-                                        headers=headers, verify=self.root, cert=self.certificate)
-                authenticate = session.get(initialconnection.url, headers=headers, verify=False)
                 response = session.get('https://{0}/{1}&xrfkey={2}'.format (self.server, endpoint, xrf),
                                         headers=headers, verify=self.root, cert=self.certificate)
                 return (response.content)
             else:
-                initialconnection = session.get('https://{0}/{1}?xrfkey={2}'.format (self.server, endpoint, xrf),
-                                        headers=headers, verify=self.root, cert=self.certificate)
-                authenticate = session.get(initialconnection.url, headers=headers, verify=False)
                 response = session.get('https://{0}/{1}?xrfkey={2}'.format (self.server, endpoint, xrf),
                                         headers=headers, verify=self.root, cert=self.certificate)
                 return (response.content)
         else:
-            initialconnection = session.get("https://{0}/{1}?filter={2} '{3}'&xrfkey={4}".format 
-                                    (self.server, endpoint, filterparam, filtervalue, xrf), 
-                                    headers=headers, verify=self.root, cert=self.certificate)
-            authenticate = session.get(initialconnection.url, headers=headers, verify=False)
             response = session.get("https://{0}/{1}?filter={2} '{3}'&xrfkey={4}".format 
                                     (self.server, endpoint, filterparam, filtervalue, xrf), 
                                     headers=headers, verify=self.root, cert=self.certificate)
@@ -221,7 +211,7 @@ class ConnectQlik:
     def get_task(self, filterparam=None, filtervalue=None):
         return (json.loads(self.get('qrs/task', filterparam, filtervalue)))
 
-    def get_securityrule(self, filterparam=None, filtervalue=None):
+    def get_systemrule(self, filterparam=None, filtervalue=None):
         return (json.loads(self.get('qrs/systemrule', filterparam, filtervalue)))
 
     def get_userdirectory(self, filterparam=None, filtervalue=None):
@@ -417,8 +407,9 @@ class ConnectQlik:
     def export_app(self, appid, filepath, filename):
         exportticket = self.get_exportappticket(appid)
         ticket = (exportticket['value'])
+        data = self.get('qrs/download/app/{0}/{1}/{2}'.format (appid, ticket, filename))
         with open(filepath + filename, 'wb') as file:
-            file.write(self.get('qrs/download/app/{0}/{1}/{2}'.format (appid, ticket, filename)))
+            file.write(data)
         return ('Application: {0} written to {1}'.format (filename, filepath))
 
     def ping_proxy(self):
@@ -427,6 +418,13 @@ class ConnectQlik:
         except requests.exceptions.RequestException as exception:
             return ('Qlik Sense Proxy down')
 
+    def new_systemrule(self, filename):
+        with open(self.concsvjson(filename), 'rb') as systemrule:
+            rule = json.loads(systemrule.read())
+            data = json.dumps(rule)
+            print (data)
+            return self.post('qrs/systemrule', data)
+
 if __name__ == '__main__':
     qrs = ConnectQlik(server='qs2.qliklocal.net:4242', 
                     certificate=('C:/certs/qs2.qliklocal.net/client.pem',
@@ -434,3 +432,4 @@ if __name__ == '__main__':
                     root='C:/certs/qs2.qliklocal.net/root.pem')
     if qrs.ping_proxy() == 200:
         print(qrs.get_about())
+
